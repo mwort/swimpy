b280461cf72fcb918938f10af32458210a1d731e
Michel Wortmann <git@michelwortmann.de>
2018-11-08 23:29:50 +0100

    Add swimpy and swim repository urls.

--- swimpy/__init__.py
+++ swimpy/__init__.py
@@ -8,3 +8,5 @@ __author__ = """Michel Wortmann"""
 __email__ = 'wortmann@pik-potsdam.de'
 __version__ = '0.1.0'
 __docs__ = 'http://www.pik-potsdam.de/~wortmann/swimpy'
+__url__ = 'https://gitlab.pik-potsdam.de/wortmann/swimpy.git'
+swim_url = 'https://gitlab.pik-potsdam.de/swim/swim.git'
