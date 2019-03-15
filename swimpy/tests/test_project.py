641217ef92d72404c20e2a654ad0283381abcf59
Michel Wortmann <git@michelwortmann.de>
2019-03-15 11:00:52 +0100

    Fix timeout to time argument change.

--- swimpy/tests/test_project.py
+++ swimpy/tests/test_project.py
@@ -51,14 +51,14 @@ class Processing:
         self.project.config_parameters(nbyr=2)
         args = [dict(smrate=i) for i in [0.1, 0.3, 0.6]]
         runs = self.project.cluster.run_parallel(
-                clones=2, args=args, prefix='test', timeout=dict(minutes=1))
+                clones=2, args=args, prefix='test', time=1)
         self.assertEqual(runs.count(), 3)
         clones = [self.project.clone[c] for c in self.project.clone.names()
                   if c.startswith('test')]
         self.assertEqual(len(clones), 2)
         # just run clones again
         runs2 = self.project.cluster.run_parallel(
-                    clones, prefix='test2', timeout={'minutes': 1})
+                    clones, prefix='test2', time=1)
         self.assertEqual(runs2.count(), 2)
         self.project.config_parameters(nbyr=oyrs)
 
