774dec24d3c546259a3b4adf7d226c648c6a4d7c
Michel Wortmann <git@michelwortmann.com>
2022-07-04 09:50:56 +0100

    Add dash-leaflet to requirements.

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         "dashboard": [
             "dash",
             "dash_bootstrap_components",
+            "dash-leaflet",
             "diskcache",
             "psutil",
             "multiprocess",
